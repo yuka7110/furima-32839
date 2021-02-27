d08a4efa09e845ee59db9ba6277a1701bb0cfe18
yuka7110 <yuka.o.nep@gmail.com>
2021-02-27 13:24:02 +0900

    first,lastの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
-  validates :birthday,        presence: true
+  validates :nickname,         presence: true
+  validates :last_name,        presence: true
+  validates :first_name,       presence: true
+  validates :last_name_kana,   presence: true
+  validates :first_name_kana,  presence: true
+  validates :birthday,         presence: true
 
   has_many :buys
   has_many :items
