c7930b3457f998be36c1686e549115e05d2ed930
yuka7110 <yuka.o.nep@gmail.com>
2021-03-03 01:08:10 +0900

    ActiveStorage導入(画像投稿)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :move_to_login, except: [:index] 
+
   def index
+    @items = Item.includes(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :fee_id, :prefecture_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
