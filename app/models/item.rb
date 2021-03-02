32ab984b6ddcf8ed2fbc8c0373bcfc66237cbceb
yuka7110 <yuka.o.nep@gmail.com>
2021-03-03 00:31:01 +0900

    出品機能のバリデーション

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :category_id,   presence: true
+  validates :condition_id,  presence: true
+  validates :fee_id,        presence: true
+  validates :prefecture_id, presence: true
+  validates :day_id,        presence: true
+  validates :price,         presence: true
+
+  belongs_to :user
+  has_one_attached :image
+
+end
