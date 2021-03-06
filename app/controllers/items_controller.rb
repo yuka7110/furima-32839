da4a0f2a60d946d917b0f28cccf5f1a1d0add300
yuka7110 <yuka.o.nep@gmail.com>
2021-03-06 17:53:14 +0900

    商品一覧表示indexの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
-# def index
-#    @items = Item.includes(:user)
-#  end
+  def index
+    @items = Item.includes(:user).order("created_at DESC")
+  end
 
   def new
     @item = Item.new
