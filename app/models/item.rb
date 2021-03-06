710947bf5b1970270a33f6d917a61d9061db3578
yuka7110 <yuka.o.nep@gmail.com>
2021-03-06 12:23:17 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,19 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
 
-  validates :image,         presence: true
-  validates :name,          presence: true
-  validates :description,   presence: true
-  validates :category_id,   numericality: { other_than: 1 }
-  validates :condition_id,  numericality: { other_than: 1 }
-  validates :fee_id,        numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :day_id,        numericality: { other_than: 1 }
+  withoptions numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id,
+    validates :prefecture_id
+    validates :day_id
+  end
+
+  withoptions presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end
 
   with_options presence: true,
                format: { with: /\A[0-9]+\z/, greater_than: 300, less_than: 10_000_000,
