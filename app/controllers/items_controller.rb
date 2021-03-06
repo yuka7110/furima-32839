7d4d339d00874332ab98309bb35847685ccb860b
yuka7110 <yuka.o.nep@gmail.com>
2021-03-06 18:12:57 +0900

    viewsの修正とrubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.includes(:user).order("created_at DESC")
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -24,5 +24,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :fee_id, :prefecture_id, :day_id, :price,
                                  :image).merge(user_id: current_user.id)
   end
-
 end
