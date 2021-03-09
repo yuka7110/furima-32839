b6bdb0e9624adc43cd93070734e1b14ce3eb32c6
yuka7110 <yuka.o.nep@gmail.com>
2021-03-09 21:44:06 +0900

    ルーティングとコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :index]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
