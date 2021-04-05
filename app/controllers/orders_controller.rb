9c2a814881688a0b52ea88e37e14c60b1a36e8e1
yuka7110 <yuka.o.nep@gmail.com>
2021-04-05 11:12:36 +0900

    購入売却コントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,9 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id
+    if current_user.id == @item.user_id || @item.order.present?
+      redirect_to root_path
+    end 
     @address_order = AddressOrder.new
   end
 
