e742cc0a9b7ba4e18da72ddabbe99cf2300b11d8
yuka7110 <yuka.o.nep@gmail.com>
2021-04-08 21:13:04 +0900

    購入コントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,7 +25,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_root
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+    redirect_to root_path if current_user.id == @item.user_id || @item.order != nil
   end
 
   def address_order_params
